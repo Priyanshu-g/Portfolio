4dbc24028b1118d59e3f43fec8918794b31a5e7a
Priyanshu Ghosh <priyanshug02@gmail.com>
2023-12-22 22:03:06 -0500

    fix

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 function dragMouseDown(e) {
-    //e = e || window.event;
+    e = e || window.event;
     //e.preventDefault();
     // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
+    pos3 = e.pageX ;
+    pos4 = e.pageY ;
+    e.target.onmouseup = closeDragElement;
     // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
+    e.target.onmousemove = elementDrag;
+}
 
-  function elementDrag(e) {
+function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
     // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
+    pos1 = pos3 - e.pageX;
+    pos2 = pos4 - e.pageY;
+    pos3 = e.pageX;
+    pos4 = e.pageY;
     // set the element's new position:
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
+}
 
-  function closeDragElement() {
+function closeDragElement(e) {
     /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+    e.target.onmouseup = null;
+    e.target.onmousemove = null;
+}
 
   
