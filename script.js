f58dab6f340fd04dbe90036d6c5528e4dce378f4
Priyanshu Ghosh <priyanshug02@gmail.com>
2023-12-22 21:51:37 -0500

    thgis sis btos emtyu

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-function dragElement(elmnt) {
-  
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
+function dragMouseDown(e) {
+    //e = e || window.event;
+    //e.preventDefault();
     // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
@@ -10,11 +8,8 @@ function dragElement(elmnt) {
     // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
-  elmnt.onmousedown = dragMouseDown;
-}
 
-function onDown(e) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
     // calculate the new cursor position:
@@ -25,12 +20,12 @@ function onDown(e) {
     // set the element's new position:
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-}
+  }
 
-function closeDragElement() {
-    // stop moving when mouse button is released:
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
     document.onmouseup = null;
     document.onmousemove = null;
-}
+  }
 
   
